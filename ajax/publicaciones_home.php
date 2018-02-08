0736d0c8da042da907ab9855bffe6da676c8a62f
Ing. Kendry Ortiz <kendry.ortiz@insignia.com.ve>
2018-02-08 11:22:43 -0400

    Fix a bug de orden de fechas de las publicaciones

--- ajax/publicaciones_home.php
+++ ajax/publicaciones_home.php
@@ -77,7 +77,7 @@ LEFT JOIN areas_sectores t5 ON t4.id_sector=t5.id
 LEFT JOIN areas t6 ON t5.id_area=t6.id 
 LEFT JOIN imagenes t7 ON t2.id_imagen=t7.id
 ".$condicion."
-ORDER BY plan DESC limit ".$_POST['pag'].",10";
+ORDER BY t1.fecha_actualizacion DESC,plan DESC limit ".$_POST['pag'].",10";
 $datos_publicaciones = $base->getAll($sql);
 
 //echo $sql;
