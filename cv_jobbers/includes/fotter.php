37974af86941ba14a7bef7c2f38f413f37d8e85a
Victor Fernandez <ingvictorfernandezs@gmail.com>
2018-03-26 11:55:52 -0430

    26-03-2018

--- cv_jobbers/includes/fotter.php
+++ cv_jobbers/includes/fotter.php
@@ -5,4 +5,7 @@ $pdf->SetY(0);
 $pdf->SetFont('Arial','I',8);
     // Print centered page number
 $pdf->Cell(0,10,''.utf8_decode('Página ').''.$pdf->PageNo(),0,0,'R');
+$pdf->Image('http://www.jobbersargentina.net/img/logo_d.png' , 77 ,272, 50 , 15,'PNG', '');
+$pdf->SetFont('Arial','',8); 
+$pdf->Text(43,288,"".utf8_decode('Cree en tí, como creemos nosotros. Jobbers el mejor portal para encontrar lo que buscas.').""); 
 ?>
--- cv_jobbers/includes/logo_jobbers.php
+++ cv_jobbers/includes/logo_jobbers.php
@@ -1,4 +1,4 @@
 <?php
-$pdf->Image('http://www.jobbersargentina.net/img/logo_d.png' , 77 ,267, 50 , 15,'PNG', '');
-
+$pdf->Image('http://www.jobbersargentina.net/img/logo_d.png' , 77 ,272, 50 , 15,'PNG', '');
+$pdf->Text(43,288,"".utf8_decode('Cree en tí, como creemos nosotros. Jobbers el mejor portal para encontrar lo que buscas.').""); 
 ?>
