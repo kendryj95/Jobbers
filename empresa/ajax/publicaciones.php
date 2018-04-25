60a259e4f5857655e0410a3b43e3c6e702f245b7
Victor Fernandez <ingvictorfernandezs@gmail.com>
2018-04-24 22:44:11 -0430

    fix visualizacion de oferta desde el panel de empresa

--- empresa/ajax/publicaciones.php
+++ empresa/ajax/publicaciones.php
@@ -44,7 +44,7 @@
 								TABLE_SCHEMA = 'db678638694'
 							AND TABLE_NAME = 'publicaciones'
 						");
-						
+
 						$sql="
 							INSERT INTO publicaciones (
 								id,
@@ -55,7 +55,7 @@
 								fecha_creacion,
 								fecha_actualizacion,
 								coordenadas,
-								ubicacion, 
+								ubicacion,
 								disponibilidad,
 								provincia,
 								localidad
@@ -70,13 +70,13 @@
 								'" . date('Y-m-d H:i:s') . "',
 								'" . date('Y-m-d H:i:s') . "',
 								'$coordenadas',
-								'',								
+								'',
 								'$info[disponibilidad]',
 								 $info[provincia],
 								 $info[localidad]
 							)";
 						$db->query($sql);
-						
+
 						$db->query("
 							INSERT INTO publicaciones_sectores (
 								id_publicacion,
@@ -87,7 +87,7 @@
 								'$info[sector]'
 							)
 						");
-						
+
 						$publicacion = $db->getRow("
 							SELECT
 								*
@@ -96,7 +96,7 @@
 							WHERE
 								id = $id
 						");
-						
+
 						$db->commitTransaction();
 
 						echo json_encode(array(
@@ -116,7 +116,7 @@
 					);
 				}
 
-				
+
 				break;
 			case MODIFICAR:
 				$id = isset($_REQUEST["i"]) ? json_decode($_REQUEST["i"], true) : false;
@@ -126,29 +126,29 @@
 
 				try {
 					if($id && $info) {
-						
+
 						$coordenadas = "";
 						if($info["latitud"] != "" && $info["latitud"] != null  && $info["longitud"] != "" && $info["longitud"] != null) {
 							$coordenadas = "$info[latitud],$info[longitud]";
 						}
 						$sql="
 							UPDATE publicaciones
-							SET 
+							SET
 							titulo = '$info[titulo]',
 							descripcion = '$info[descripcion]',
 							amigable = '" . slug($info["titulo"]) . "',
 							fecha_actualizacion = '" . date('Y-m-d H:i:s') . "',
-							coordenadas='$coordenadas', 
+							coordenadas='$coordenadas',
 							provincia=$info[provincia],
 							localidad=$info[localidad],
 							disponibilidad=$info[disponibilidad]
 							WHERE
 								id = $id
 						";
-						
+
 
 						$db->query($sql);
-						
+
 						$db->query("
 							DELETE
 							FROM
@@ -156,7 +156,7 @@
 							WHERE
 								id_publicacion = $id
 						");
-						
+
 						$db->query("
 							INSERT INTO publicaciones_sectores (
 								id_publicacion,
@@ -167,7 +167,7 @@
 								'$info[sector]'
 							)
 						");
-						
+
 						$publicacion = $db->getRow("
 							SELECT
 								*
@@ -178,7 +178,7 @@
 						");
 
 						$db->commitTransaction();
-						
+
 						echo json_encode(array(
 							"msg" => "OK",
 							"data" => array(
@@ -196,7 +196,7 @@
 					);
 				}
 
-				
+
 				break;
 			case ELIMINAR:
 				$id = isset($_REQUEST["i"]) ? json_decode($_REQUEST["i"], true) : false;
@@ -228,7 +228,7 @@
 						)
 					);
 				}
-				
+
 				break;
 			case CARGAR_TODAS:
 				$publicaciones = array();
@@ -274,7 +274,7 @@
                 $plan = $db->getRow("SELECT id_plan FROM empresas_planes WHERE id_empresa=".$_SESSION['ctc']['empresa']['id']);
 
                 if($datos) {
-					$datos = array_reverse($datos);					
+					$datos = array_reverse($datos);
 					foreach($datos as $k => $pub) {
 						$pub["link_postulados"] = $pub["postulados"] > 0 ? ('<a value="'.$pub["titulo"].'" class="text-primary" href="javascript: void(0);" data-toggle="modal" data-target="#modal-postulados" data-id="' . $pub["id"] . '"><span class="underline">' . $pub["postulados"] . ' trabajador(es)</span></a>') : "";
 
@@ -293,11 +293,11 @@
                                 		$db->query("UPDATE publicaciones SET estatus=0 WHERE id=".$pub["id"]);
                                 	}
                                 }
-                                
+
                                 $startStopPub = $pub["estatus"] == 1 ? '<button type="button" class="accion-publicacion btn btn-default waves-effect waves-light" title="Detener publicación" onclick="stopStartPub(this,0)"><i class="fa fa-pause"></i></button>' : '<button type="button" class="accion-publicacion btn btn-default waves-effect waves-light" title="Renaudar publicación" onclick="stopStartPub(this,1)"><i class="fa fa-play"></i></button>';
 
-                                $accion = $timestamp_today <= $timestamp_final ? '<div class="acciones-publicacion btn-group" role="group" data-target="' . $pub["id"] . '"> <a class="accion-publicacion btn btn-success waves-effect waves-light" title="Previsualizar publicación" href="../empleos-detalle.php?a=' . $pub["area_amigable"] . '&s=' . $pub["sector_amigable"] . '&p=' . $pub["amigable"] . '"><i class="icon icon-eye-plus"></i></a> <button type="button" class="accion-publicacion btn btn-primary waves-effect waves-light" onclick="modificarPublicacion(this);" title="Modificar publicación"><i class="fa fa-pencil-square-o"></i></button> <button type="button" class="accion-publicacion btn btn-danger waves-effect waves-light" title="Eliminar publicación" onclick="eliminarPublicacion(this);"><i class="fa fa-trash"></i></button> ' . $startStopPub . '</div>' : '
-                                	<div class="acciones-publicacion btn-group" role="group" data-target="' . $pub["id"] . '"> 
+                                $accion = $timestamp_today <= $timestamp_final ? '<div class="acciones-publicacion btn-group" role="group" data-target="' . $pub["id"] . '"> <a class="accion-publicacion btn btn-success waves-effect waves-light" title="Previsualizar publicación" href="../empleos-detalle.php?a=' . $pub["area_amigable"] . '&s=' . $pub["sector_amigable"] . '&p=' . $pub["amigable"] . '&e='.$_SESSION['ctc']['id'].'"><i class="icon icon-eye-plus"></i></a> <button type="button" class="accion-publicacion btn btn-primary waves-effect waves-light" onclick="modificarPublicacion(this);" title="Modificar publicación"><i class="fa fa-pencil-square-o"></i></button> <button type="button" class="accion-publicacion btn btn-danger waves-effect waves-light" title="Eliminar publicación" onclick="eliminarPublicacion(this);"><i class="fa fa-trash"></i></button> ' . $startStopPub . '</div>' : '
+                                	<div class="acciones-publicacion btn-group" role="group" data-target="' . $pub["id"] . '">
                                 	<a class="accion-publicacion btn btn-success waves-effect waves-light" title="Previsualizar publicación" href="../empleos-detalle.php?a=' . $pub["area_amigable"] . '&s=' . $pub["sector_amigable"] . '&p=' . $pub["amigable"] . '&e='.$pub["id_empresa"].'"><i class="icon icon-eye-plus"></i></a> <button type="button" class="accion-publicacion btn btn-primary waves-effect waves-light" onclick="renovarPublicacion(this);" title="Renovar publicación"><i class="fa fa-retweet"></i></button> <button type="button" class="accion-publicacion btn btn-danger waves-effect waves-light" title="Eliminar publicación" onclick="eliminarPublicacion(this);"><i class="fa fa-trash"></i></button> </div> ';
 
                                 $caducada = $timestamp_today <= $timestamp_final ? '' : '<br><span class="label label-danger">OFERTA CADUCADA</span>';
@@ -329,7 +329,7 @@
                                 $startStopPub = $pub["estatus"] == 1 ? '<button type="button" class="accion-publicacion btn btn-default waves-effect waves-light" title="Detener publicación" onclick="stopStartPub(this,0)"><span class="ti-hand-stop"></span></button>' : '<button type="button" class="accion-publicacion btn btn-default waves-effect waves-light" title="Renaudar publicación" onclick="stopStartPub(this,1)"><span class="ti-control-play"></span></button>';
 
                                 $accion = $timestamp_today <= $timestamp_final ? '<div class="acciones-publicacion btn-group" role="group" data-target="' . $pub["id"] . '"> <a class="accion-publicacion btn btn-success waves-effect waves-light" title="Previsualizar publicación" href="../empleos-detalle.php?a=' . $pub["area_amigable"] . '&s=' . $pub["sector_amigable"] . '&p=' . $pub["amigable"] . '&e='.$pub["id_empresa"].'"><i class="icon icon-eye-plus"></i></a> <button type="button" class="accion-publicacion btn btn-primary waves-effect waves-light" onclick="modificarPublicacion(this);" title="Modificar publicación"><i class="fa fa-pencil-square-o"></i></button> <button type="button" class="accion-publicacion btn btn-danger waves-effect waves-light" title="Eliminar publicación" onclick="eliminarPublicacion(this);"><i class="fa fa-trash"></i></button> ' . $startStopPub . '</div>' : '
-                                	<div class="acciones-publicacion btn-group" role="group" data-target="' . $pub["id"] . '"> 
+                                	<div class="acciones-publicacion btn-group" role="group" data-target="' . $pub["id"] . '">
                                 	<a class="accion-publicacion btn btn-success waves-effect waves-light" title="Previsualizar publicación" href="../empleos-detalle.php?a=' . $pub["area_amigable"] . '&s=' . $pub["sector_amigable"] . '&p=' . $pub["amigable"] . '"><i class="icon icon-eye-plus"></i></a> <button type="button" class="accion-publicacion btn btn-primary waves-effect waves-light" onclick="renovarPublicacion(this);" title="Renovar publicación"><i class="fa fa-retweet"></i></button> <button type="button" class="accion-publicacion btn btn-danger waves-effect waves-light" title="Eliminar publicación" onclick="eliminarPublicacion(this);"><i class="fa fa-trash"></i></button> </div> ';
 
                                 $caducada = $timestamp_today <= $timestamp_final ? '' : '<br><span class="label label-danger">OFERTA CADUCADA</span>';
@@ -361,7 +361,7 @@
                                 $startStopPub = $pub["estatus"] == 1 ? '<button type="button" class="accion-publicacion btn btn-default waves-effect waves-light" title="Detener publicación" onclick="stopStartPub(this,0)"><span class="ti-hand-stop"></span></button>' : '<button type="button" class="accion-publicacion btn btn-default waves-effect waves-light" title="Renaudar publicación" onclick="stopStartPub(this,1)"><span class="ti-control-play"></span></button>';
 
 								$accion = $timestamp_today <= $timestamp_final ? '<div class="acciones-publicacion btn-group" role="group" data-target="' . $pub["id"] . '"> <a class="accion-publicacion btn btn-success waves-effect waves-light" title="Previsualizar publicación" href="../empleos-detalle.php?a=' . $pub["area_amigable"] . '&s=' . $pub["sector_amigable"] . '&p=' . $pub["amigable"] . '&e='.$pub["id_empresa"].'"><i class="icon icon-eye-plus"></i></a> <button type="button" class="accion-publicacion btn btn-primary waves-effect waves-light" onclick="modificarPublicacion(this);" title="Modificar publicación"><i class="fa fa-pencil-square-o"></i></button> <button type="button" class="accion-publicacion btn btn-danger waves-effect waves-light" title="Eliminar publicación" onclick="eliminarPublicacion(this);"><i class="fa fa-trash"></i></button> ' . $startStopPub . '</div>' : '
-								<div class="acciones-publicacion btn-group" role="group" data-target="' . $pub["id"] . '"> 
+								<div class="acciones-publicacion btn-group" role="group" data-target="' . $pub["id"] . '">
 								<a class="accion-publicacion btn btn-success waves-effect waves-light" title="Previsualizar publicación" href="../empleos-detalle.php?a=' . $pub["area_amigable"] . '&s=' . $pub["sector_amigable"] . '&p=' . $pub["amigable"] . '"><i class="icon icon-eye-plus"></i></a> <button type="button" class="accion-publicacion btn btn-primary waves-effect waves-light" onclick="renovarPublicacion(this);" title="Renovar publicación"><i class="fa fa-retweet"></i></button> <button type="button" class="accion-publicacion btn btn-danger waves-effect waves-light" title="Eliminar publicación" onclick="eliminarPublicacion(this);"><i class="fa fa-trash"></i></button> </div> ';
 
 							$caducada = $timestamp_today <= $timestamp_final ? '' : '<br><span class="label label-danger">OFERTA CADUCADA</span>';
@@ -377,7 +377,7 @@
                                         $accion,
                                     );
                                 // }
-                            	break;    
+                            	break;
                             default: // Plan Oro
 
                             	$timestamp_final = strtotime("+35 day", strtotime($pub["fecha_actualizacion"]));
@@ -394,7 +394,7 @@
                             	$startStopPub = $pub["estatus"] == 1 ? '<button type="button" class="accion-publicacion btn btn-default waves-effect waves-light" title="Detener publicación" onclick="stopStartPub(this,0)"><i class="fa fa-pause"></i></button>' : '<button type="button" class="accion-publicacion btn btn-default waves-effect waves-light" title="Renaudar publicación" onclick="stopStartPub(this,1)"><i class="fa fa-play"></i></button>';
 
                             	$accion = $timestamp_today <= $timestamp_final ? '<div class="acciones-publicacion btn-group" role="group" data-target="' . $pub["id"] . '"> <a class="accion-publicacion btn btn-success waves-effect waves-light" title="Previsualizar publicación" href="../empleos-detalle.php?a=' . $pub["area_amigable"] . '&s=' . $pub["sector_amigable"] . '&p=' . $pub["amigable"] . '&e='.$pub["id_empresa"].'"><i class="icon icon-eye-plus"></i></a> <button type="button" class="accion-publicacion btn btn-primary waves-effect waves-light" onclick="modificarPublicacion(this);" title="Modificar publicación"><i class="fa fa-pencil-square-o"></i></button> <button type="button" class="accion-publicacion btn btn-danger waves-effect waves-light" title="Eliminar publicación" onclick="eliminarPublicacion(this);"><i class="fa fa-trash"></i></button> ' . $startStopPub . '</div>' : '
-                                	<div class="acciones-publicacion btn-group" role="group" data-target="' . $pub["id"] . '"> 
+                                	<div class="acciones-publicacion btn-group" role="group" data-target="' . $pub["id"] . '">
                                 	<a class="accion-publicacion btn btn-success waves-effect waves-light" title="Previsualizar publicación" href="../empleos-detalle.php?a=' . $pub["area_amigable"] . '&s=' . $pub["sector_amigable"] . '&p=' . $pub["amigable"] . '"><i class="icon icon-eye-plus"></i></a> <button type="button" class="accion-publicacion btn btn-primary waves-effect waves-light" onclick="renovarPublicacion(this);" title="Renovar publicación"><i class="fa fa-retweet"></i></button> <button type="button" class="accion-publicacion btn btn-danger waves-effect waves-light" title="Eliminar publicación" onclick="eliminarPublicacion(this);"><i class="fa fa-trash"></i></button> </div> ';
 
                                 $caducada = $timestamp_today <= $timestamp_final ? '' : '<br><span class="label label-danger">OFERTA CADUCADA</span>';
@@ -413,7 +413,7 @@
 
 					}
 				}
-				
+
 				echo json_encode($publicaciones);
 				break;
 			case DETALLES:
@@ -431,7 +431,7 @@
 						WHERE
 							p.id = $id
 					");
-					
+
 					echo json_encode(array(
 						"msg" => "OK",
 						"data" => array(
@@ -447,7 +447,7 @@
 				$plan = $db->getRow("SELECT id_plan FROM empresas_planes WHERE id_empresa=".$_SESSION['ctc']['empresa']['id']);
 
 				$limit = '';
-				
+
 				switch ($plan['id_plan']) {
 					case 1: // Plan Gratis
 						$limit = "LIMIT 10";
@@ -459,10 +459,10 @@
 						$limit = "LIMIT 100";
 						break;
 				}
-				$sql="SELECT 
+				$sql="SELECT
 					t1.id,
-					t2.id_trabajador, 
-					t3.uid AS uid_trabajador,  
+					t2.id_trabajador,
+					t3.uid AS uid_trabajador,
 					t3.id_sexo,
 					t2.fecha_hora,
 					t3.provincia,
@@ -485,30 +485,30 @@
 
 					LEFT JOIN trabajadores_experiencia_laboral t9 ON t9.id_trabajador = t3.id
 					LEFT JOIN actividades_empresa t10 ON t10.id = t9.id_actividad_empresa
-					                
+
 					WHERE t1.id=".$id."
 					GROUP by t3.id $limit";
 
 
-			 
+
 				$datos = $db->getAll($sql);
 
-				
+
 
 				if($datos) {
-				
+
 					 $calificar = array(
 					 	'' => '',
-					 	'1' => '★', 
-					 	'2' => '★★', 
-					 	'3' => '★★★', 
-					 	'4' => '★★★★', 
-					 	'5' => '★★★★★',  
-					 	);					 
+					 	'1' => '★',
+					 	'2' => '★★',
+					 	'3' => '★★★',
+					 	'4' => '★★★★',
+					 	'5' => '★★★★★',
+					 	);
 					foreach($datos as $k => $fila) {
-						 
+
 						$sexos = array(1 => "M", 2 => "F");
-						$marcadores = array("" => "Normal", 
+						$marcadores = array("" => "Normal",
 							"0" => "Descartado",
 							"1" => "Contactado",
 							"2" => "En proceso",
@@ -523,11 +523,11 @@
 							'<a target="_blank" style="font-size:12px;" href="../trabajador-detalle.php?t=' . $fila["id_trabajador"] . '&pubid=' . $fila["id"] . '"><strong>' . "$fila[nombre]" . '</strong></a>
 							<div style="color: #ffde00;font-size:14px;">'.$calificar[$fila['calificacion']].'</div>
 							<div style="font-size:11px;">
-							<strong>Edad: </strong>' . $fila["edad"] . '<strong> 
+							<strong>Edad: </strong>' . $fila["edad"] . '<strong>
 							Sexo: </strong>' . $sexos[$fila["id_sexo"] ]. '
-							 
-							</div> 
-							',						 
+
+							</div>
+							',
 							$fila['edad'],
 							$fila['id_area_estudio'],
 							$fila['provincia'],
@@ -538,18 +538,18 @@
 							$fila['actividad_empresa'],
 							$fila['marcador'],
 							$fila['fecha_hora'],
-							'<div class="acciones-publicacion text-center" data-target="' . $fila["id_trabajador"] . '"> 
+							'<div class="acciones-publicacion text-center" data-target="' . $fila["id_trabajador"] . '">
 							 <strong></strong>'. $marcadores[$fila["marcador"]].'
-							</div>',					
+							</div>',
 						);
 					}
-				} 
+				}
 				echo json_encode(array(
-					"data" => $postulados, 
+					"data" => $postulados,
 				));
 				break;
 			case AGREGAR_ESPECIAL:
-				
+
 				$db->beginTransaction();
 
 				if(isset($_REQUEST["edit"])) {
@@ -573,7 +573,7 @@
 							$db->rollBackTransaction();
 							$t = 3;
 						}
-						
+
 					}
 					else {
 
@@ -603,7 +603,7 @@
 				else {
 					$id = $db->getOne("SELECT AUTO_INCREMENT FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'db678638694' AND TABLE_NAME = 'empresas_publicaciones_especiales'");
 					if(isset($_REQUEST["video"])) {
-						
+
 						try{
 							$db->query("INSERT INTO empresas_publicaciones_especiales (id, id_empresa, tipo, titulo, descripcion, id_imagen, enlace, fecha_creacion) VALUES ($id, '".$_SESSION["ctc"]["id"]."', 2, '$_REQUEST[titulo]', '', 0, '$_REQUEST[url]', '".date('Y-m-d')."');");
 							$t = 1;
@@ -652,7 +652,7 @@
 					echo json_encode(array("msg" => "OK"));
 				} else {
 					$pub = $db->getRow("SELECT COUNT(*) AS pub FROM publicaciones WHERE id_empresa=".$_SESSION['ctc']['id']);
-					
+
 					if ($pub['pub'] >= 2){
 						echo json_encode(array("msg" => "NO"));
 					} else {
@@ -707,8 +707,8 @@
 						)
 					);
 				}
-				break;	
-				
+				break;
+
 		}
 	}
 	function getExtension($str) {$i=strrpos($str,".");if(!$i){return"";}$l=strlen($str)-$i;$ext=substr($str,$i+1,$l);return $ext;}
